24061e5e00f7f4b40f0e29d58aecff817a114480
Faturahman-Dev <agungfaturahman323@gmail.com>
2026-03-23 16:10:06 +0700

    Fix prisma config

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,12 +1,5 @@
-import "dotenv/config";
-import { defineConfig } from "@prisma/config";
+import { defineConfig } from "prisma/config";
 
 export default defineConfig({
-  // Kasih tau Prisma letak file schema lu
   schema: "prisma/schema.prisma",
-
-  // Ganti env bawaan pakai process.env bawaan Node
-  datasource: {
-    url: process.env.DATABASE_URL!,
-  },
 });
